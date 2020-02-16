c158eaf8ec445837b198b396af2a26a86a0c79c5
priyamvada <pranshulpandey5@gmail.com>
2020-02-16 14:52:27 +0000

    adding the new template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalModule } from 'ngb-modal';
+import { MDBBootstrapModule} from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FrontPageComponent} from './front-page/frontPage.component';
-import { CardVideoComponent} from './cardVideo/cardVideo.component';
-import { CardItemComponent} from './cardVideo/cardItem/cardItem.component';
+import { MainLayoutComponent} from './main-layout/mainLayout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FrontPageComponent,
-    CardVideoComponent,
-    CardItemComponent
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
-    ModalModule
+    ModalModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-layout/mainLayout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mainLayout',
+  templateUrl: './mainLayout.component.html',
+  styleUrls: ['./mainLayout.component.scss']
+})
+export class MainLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
